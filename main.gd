212adfb97a35e95fa8701d0c428398d5d3a31610
xXCat-8Xx <sebastian.gnoyke@gmx.net>
2025-04-02 10:06:37 +0200

    Pause Menü
    das Pause Menü funktioniert nun

--- main.gd
+++ main.gd
@@ -1,17 +1,17 @@
 extends Node3D
-@onready var pause_menü = $"Node3D/Pause Menü"
+
+@onready var pause_menü = $"Node3D/Pause Menü"  # Referenz auf das Pause-Menü
 var pausiert = false
 
-func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("pause"):
-		PauseMenü()
+func _process(_delta: float) -> void:
+	if Input.is_action_just_pressed("Pause"):  # Überprüfe, ob die "Pause"-Taste gedrückt wurde
+		PauseMenü()  # Aufruf der Funktion zum Pausieren oder Fortsetzen des Spiels
 
 func PauseMenü():
 	if pausiert:
-		pause_menü.hide()
-		Engine.time_scale = 1
-		
+		pause_menü.hide()  # Verstecke das Menü
+		Engine.time_scale = 1  # Setze die Zeit zurück, sodass das Spiel weiterläuft
 	else:
-		pause_menü.show()
-		Engine.time_scale = 0
-	pausiert = !pausiert
+		pause_menü.show()  # Zeige das Menü
+		Engine.time_scale = 0  # Pausiere das Spiel
+	pausiert = !pausiert  # Wechsle den Pausenstatus
